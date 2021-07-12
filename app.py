e1894b910312940aeab6f32b194cdaa92b88ee3c
pandoraje <87154174+pandoraje@users.noreply.github.com>
2021-07-12 11:30:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,5 @@ application = Flask(__name__)
 def main():
 	return "Hello!"
 
-@application.route('/how are you')
-def hello():
-	return 'I am good, how about you?'
-
 if __name__ == "__main__":
 	application.run()
